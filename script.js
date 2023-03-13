04d58c07dcd876bf9a93a9d4d325f435e598226d
ShivamRajotiyaVirtuos <127102519+ShivamRajotiyaVirtuos@users.noreply.github.com>
2023-03-13 12:42:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  console.log("open nav");
+  document.getElementById("submenu").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("submenu").style.width = "0";
+  document.getElementById("submenu1").style.width = "0";
+  console.log("close nav");
+}
+function openSubNav() {
+  document.getElementById("submenu").style.display = "block";
+  document.getElementById("submenu").style.width = "250px";
+  console.log("submenu men");
+
+  document.getElementById("mySidenav").style.width = "0";
+}
+function openSubNav1() {
+  document.getElementById("submenu1").style.display = "block";
+  document.getElementById("submenu1").style.width = "250px";
+  console.log("submenu women");
+
+  document.getElementById("mySidenav").style.width = "0";
+}
+document.addEventListener("click", function handleClickOutsideBox(event) {
+  const mySidenav = document.getElementById("mySidenav");
+  const submenu = document.getElementById("submenu");
+  const submenu1 = document.getElementById("submenu1");
+
+  const navbar = document.getElementById("navbar");
+
+  if (mySidenav) {
+    if (
+      !navbar.contains(event.target) &&
+      !mySidenav.contains(event.target) &&
+      !submenu.contains(event.target) &&
+      !submenu1.contains(event.target)
+    ) {
+      console.log("close");
+      mySidenav.style.width = "0px";
+      submenu.style.width = "0px";
+      submenu1.style.width = "0px";
+    }
+  }
+});
